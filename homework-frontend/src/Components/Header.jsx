3eaff1a0f8b9deb9db9b242e30289175ac9926fc
uzair910 <uzair.az@gmail.com>
2024-10-30 10:24:15 +0200

    Added header. cant get the styles right. check it out later.

---
+++ homework-frontend/src/Components/Header.jsx
@@ -0,0 +1,10 @@
+import './Header.css' // copied from an older project
+
+export default function Header() {
+  return (
+    <header>
+      <h2>Homework MVC</h2>
+      <button class="btn">Home</button>
+    </header>
+  );
+}
