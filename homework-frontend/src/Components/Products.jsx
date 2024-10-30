b5962a8120fe580dbb7535d8a527581db72fbb01
uzair910 <uzair.az@gmail.com>
2024-10-30 15:05:31 +0200

    ID and Title breng displayed !

--- homework-frontend/src/Components/Products.jsx
+++ homework-frontend/src/Components/Products.jsx
@@ -6,35 +6,23 @@ export default function Products() {
   useEffect(() => {
     async function fetchProducts() {
       try {
-        const response = await fetch("http://localhost:5034/api/products", {
-          // mode: "no-cors",
-        });
-        console.log("response: ", response);
-        console.log("response OK : ", response.ok);
-        console.log(" response STATUS: ", response.status);
-
+        const response = await fetch("http://localhost:5034/api/products");
         if (!response.ok) {
           throw new Error(`HTTP error! status: ${response.status}`);
         }
-
-        console.log("response OK! / NOW fetching products...");
         const products = await response.json();
-
-        console.log("PRODCUTS: ", products);
-        console.log("setting state products...");
         setProducts(products);
       } catch (error) {
         console.error("Error fetching products:", error);
       }
     }
-
     fetchProducts();
   }, []);
 
   return (
     <ul id="products">
       {products.map((product) => (
-        <li key={product.ID}>{product.Title}</li>
+        <li key={product.id}>{product.title}</li>
       ))}
     </ul>
   );
