2ad30851deb76e23734fc83932adf4c7e7545112
uzair910 <uzair.az@gmail.com>
2024-11-01 12:19:28 +0200

    formatting

--- Homework.Api/Models/Product.cs
+++ Homework.Api/Models/Product.cs
@@ -1,16 +1,16 @@
 namespace Homework.Api.Models
 {
 
-        public interface IProduct
+    public interface IProduct
     {
         public int ID { get; set; }
         public string? Title { get; set; }
         public string? Brand { get; set; }
-        public Decimal DiscountPercentage { get; set; } 
+        public Decimal DiscountPercentage { get; set; }
         public Decimal Rating { get; set; }
         public Decimal Price { get; set; }
         public string? Description { get; set; }
-        
+
 
     }
     public class Product : IProduct
@@ -18,11 +18,11 @@ namespace Homework.Api.Models
         public int ID { get; set; }
         public string? Title { get; set; }
         public string? Brand { get; set; }
-        public Decimal DiscountPercentage { get; set; } 
+        public Decimal DiscountPercentage { get; set; }
         public Decimal Rating { get; set; }
         public Decimal Price { get; set; }
         public string? Description { get; set; }
-        
+
 
     }
 }
